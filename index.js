5ba15fe780355d21a5a6efe0f7ac1c597570cbe8
Anuradha Gupta <anuradhagwl98@gmail.com>
2024-07-31 19:26:20 +0530

    new changes

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const { Server } = require("socket.io");
 
 
 const app = express();
-const server = http.createServer(app)
+const server = http.createServer(app);
 const io = new Server(server);
 
 // Socket.io
